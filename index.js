f7161f4eca4dafe76e76dc0f23e252562952af17
TechBoxNorth <techboxnorth@gmail.com>
2024-01-10 10:11:16 +0100

    variables

---
+++ index.js
@@ -0,0 +1,13 @@
+// print text to console
+console.log("Hello World!");
+
+// the contents and datatype of a let variable can be changed
+let message = "Tjoho!";
+let number = 100;
+number = false;
+
+// the contents and datatype of a const variable can NOT be changed
+const name = "Anders";
+// name = 1000; // This will not work!
+
+console.log(`Message: ${message} Name: ${name} Number: ${number}`);
